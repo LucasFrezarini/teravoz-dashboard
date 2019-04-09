ff7a585d01dc7a621a22eac52afdc9f5bb302d00
Lucas Frezarini <lucas.frezarini@hypnobox.com.br>
2019-04-09 17:56:54 -0300

    Using Apollo Cli to fetch data from graphql backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,29 @@
 import React, { Component } from "react";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCloud } from "@fortawesome/free-solid-svg-icons";
 
 import "./App.css";
 import Container from "react-bootstrap/Container";
 import Table from "react-bootstrap/Table";
-import Navbar from "react-bootstrap/Navbar";
 import Footer from "./components/shared/Footer";
 import Header from "./components/shared/Header";
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
 
 library.add(faCloud);
+
+const DASHBOARD_QUERY = gql`
+  query {
+    calls {
+      call_id
+      their_number
+      their_number_type
+      type
+      timestamp
+    }
+  }
+`;
+
 class App extends Component {
   public render() {
     return (
@@ -19,42 +32,40 @@ class App extends Component {
         <Container className="mt-3">
           <h1>Dashboard</h1>
           <hr />
-          <Table striped>
-            <thead>
-              <tr>
-                <th>ID</th>
-                <th>Caller Number</th>
-                <th>Type</th>
-                <th>Status</th>
-                <th>Last Update</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>123456789</td>
-                <td>11967744005</td>
-                <td>Mobile</td>
-                <td>Stand By</td>
-                <td>Now</td>
-              </tr>
-              <tr>
-                <td>123456789</td>
-                <td>11967744005</td>
-                <td>Mobile</td>
-                <td>Stand By</td>
-                <td>Now</td>
-              </tr>
-              <tr>
-                <td>123456789</td>
-                <td>11967744005</td>
-                <td>Mobile</td>
-                <td>Stand By</td>
-                <td>Now</td>
-              </tr>
-            </tbody>
-          </Table>
+          <Query query={DASHBOARD_QUERY}>
+            {({ loading, error, data }: any) => {
+              if (loading) return <div>Loading</div>;
+              if (error) return <div>Error</div>;
+
+              const { calls } = data;
+
+              return (
+                <Table striped>
+                  <thead>
+                    <tr>
+                      <th>ID</th>
+                      <th>Caller Number</th>
+                      <th>Type</th>
+                      <th>Status</th>
+                      <th>Last Update</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {calls.map((call: any) => (
+                      <tr>
+                        <td>{call.call_id}</td>
+                        <td>{call.their_number}</td>
+                        <td>{call.their_number_type}</td>
+                        <td>{call.type}</td>
+                        <td>{call.timestamp}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </Table>
+              );
+            }}
+          </Query>
         </Container>
-        <Footer />
       </div>
     );
   }
--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,15 @@ import "bootstrap/dist/css/bootstrap.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import { ApolloProvider } from "react-apollo";
+import client from "./apollo/client";
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root"),
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
